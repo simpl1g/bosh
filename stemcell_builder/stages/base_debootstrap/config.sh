7fcc4c223a315365a93989a8d3a522b73a562d3e
Konstantin Ilchenko <konstantin.ilchenko@altoros.com>
2013-10-23 16:18:19 +0300

    generate precise stemcell

--- stemcell_builder/stages/base_debootstrap/config.sh
+++ stemcell_builder/stages/base_debootstrap/config.sh
@@ -23,7 +23,7 @@ base_debootstrap_arch=amd64
 
 if [ -z "${base_debootstrap_suite:-}" ]
 then
-  base_debootstrap_suite=lucid
+  base_debootstrap_suite=precise
 fi
 
 persist_value base_debootstrap_arch
